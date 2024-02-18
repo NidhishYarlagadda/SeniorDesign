e454a31e4984dad6671122d29f827bb3479b66e9
MalikSuleiman <abdulmalik.suleiman@uconn.edu>
2024-02-18 18:25:54 -0500

    FUCKING AROUND COMMIT

--- Angle Sensor/connection.py
+++ Angle Sensor/connection.py
@@ -16,16 +16,23 @@ time.sleep(0.05)
 for node_id in network.scanner.nodes:
     print("Found node %d!" % node_id)
 
-# Create board object
-nodeId = 90
-board = network[nodeId]
+#board = network.add_node(90)# needs an eds file to add node
+#msg = bytearray(0x003200401000000)
+network.send_message(90,[0x01,0x02,0x03]) # dummy message seems to successful because no error so..
+time.sleep(0.1)
+messages = network.receive(timeout=1.0) # receive doesn't work
+for message in messages:
+    print('Received message:',message)
 
-#print object dictionary
-for obj in board.object_dictionary.values():
-    print('0x%X: %s' % (obj.index, obj.name))
-    if isinstance(obj, canopen.objectdictionary.ODRecord):
-        for subobj in obj.values():
-            print('  %d: %s' % (subobj.subindex, subobj.name))
+#print(board.object_dictionary)
+
+#print object dictionary(Currently can't load anything as the object is empty)
+#for obj in board.object_dictionary.values():
+    #print(obj)
+    #print('0x%X: %s' % (obj.index, obj.name))
+    #if isinstance(obj, canopen.objectdictionary.ODRecord):
+    #    for subobj in obj.values():
+     #       print('  %d: %s' % (subobj.subindex, subobj.name))
 network.disconnect()
 
 #if __name__ == "__main__":
