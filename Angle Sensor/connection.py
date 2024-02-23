540a672695fd2b9f676b14b2cd33ef02642a0255
MalikSuleiman <abdulmalik.suleiman@uconn.edu>
2024-02-23 16:38:14 -0500

    EDS implementated, Experiment

--- Angle Sensor/connection.py
+++ Angle Sensor/connection.py
@@ -18,21 +18,35 @@ for node_id in network.scanner.nodes:
 
 #board = network.add_node(90)# needs an eds file to add node
 #msg = bytearray(0x003200401000000)
-network.send_message(90,[0x01,0x02,0x03]) # dummy message seems to successful because no error so..
-time.sleep(0.1)
-messages = network.receive(timeout=1.0) # receive doesn't work
-for message in messages:
-    print('Received message:',message)
+#network.send_message(90,[0x01,0x02,0x03]) # dummy message seems to successful because no error so..
+#time.sleep(0.1)
+#messages = network.receive(timeout=1.0) # receive doesn't work
+#for message in messages:
+ #   print('Received message:',message)
+    
+# ASSUMING THE EDS FILE ALLOWS FOR THE NODE TO BE ADDED TO THE NETWORK
+pathofeds = "as5-101.eds"
+node = network.add_node(90,pathofeds)
 
 #print(board.object_dictionary)
 
 #print object dictionary(Currently can't load anything as the object is empty)
-#for obj in board.object_dictionary.values():
+#for obj in node.object_dictionary.values():
     #print(obj)
-    #print('0x%X: %s' % (obj.index, obj.name))
-    #if isinstance(obj, canopen.objectdictionary.ODRecord):
-    #    for subobj in obj.values():
-     #       print('  %d: %s' % (subobj.subindex, subobj.name))
+ #   print('0x%X: %s' % (obj.index, obj.name))
+ #   if isinstance(obj, canopen.objectdictionary.ODRecord):
+ #      for subobj in obj.values():
+ #          print('  %d: %s' % (subobj.subindex, subobj.name))
+cal_stat = (node.sdo[0x2003][0x04]).raw
+
+cal_stat_bit = (0b11000000 & cal_stat) >> 6 #Bit mask for status bits
+
+print("Status:{:02b}".format(cal_stat_bit)) # Prints out status
+
+
+# to send messages using the sdo
+
+
 network.disconnect()
 
 #if __name__ == "__main__":
