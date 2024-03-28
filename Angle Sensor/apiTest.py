b2d09cc14c9c59e575d2b34a3680d03b77909b85
MalikSuleiman <abdulmalik.suleiman@uconn.edu>
2024-03-28 18:05:38 -0400

    Tested the DoCal

--- Angle Sensor/apiTest.py
+++ Angle Sensor/apiTest.py
@@ -2,12 +2,17 @@ from as5 import AS5
 import canopen
 
 network = canopen.Network()
+# Hardcoding which channel to use
+network.connect(bustype='pcan', channel= 'PCAN_USBBUS1', bitrate= 125000)
 
 myDevice = AS5(network)
 
+
 myDevice.DoCal()
 
-print(myDevice.getAngle(0))
+#bin(myDevice.getcalstatus())
+
+#print(myDevice.getAngle(0))
 
 
 network.disconnect()
